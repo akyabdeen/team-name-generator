2466e9d9dddff861963e85ce15368505ca2774d5
JordanAbdeen <abdeendunz@gmail.com>
2024-12-07 00:50:01 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import { FiTrash } from 'react-icons/fi';
+
 import Landing from './components/Landing';
 import TeamGenerator from './components/TeamGenerator';
 
 function App() {
-    const [showLanding, setShowLanding] = useState(true);
-    
+    const [showLanding, setShowLanding] = useState(true); // state to manage which card appears on screen
+    const [showLikedNames, setShowLikedNames] = useState(false); // state to manage if the liked names should be shown
+    const [likedNames, setLikedNames] = useState([]); // state to store the liked names
+    const [isTransitioning, setIsTransitioning] = useState(false); // state to manage the transition between the landing and team generator cards
+
+    // this function is called by the button inside the landing component to transition to the team generator card
     const transition = () => {
-        setShowLanding(false);
+        setIsTransitioning(true); // set the transitioning state to true to trigger the fade-out animation
+        setTimeout(() => {
+            setShowLanding(false);
+            setIsTransitioning(false);
+        }, 300); // the transition duration is 300ms so that it matches the fade-out duration
+    }
+
+    // this function is called by the TeamGenerator component to send the value of the liked name to the parent component of App to save it in the likedNames array
+    const handleLike = (name) => {
+        setLikedNames([...likedNames, name]);
+    }
+
+    // this function is called by the span element to toggle the visibility of the liked names
+    const toggleShowLikesNames = () => {
+        setShowLikedNames(() => !showLikedNames);
     }
 
   return (
     <div className="App">
-        {showLanding ? (
-            <div className='fade-in'>
-                <Landing handleClick={transition} />
-            </div>
-        ) : (
-            <div className='fade-in'>
-                <TeamGenerator />
-            </div>
-        )}
+        <div className='liked-names-container'>
+            <span className='show-names' onClick={toggleShowLikesNames}>
+                {!showLikedNames ? "Show" : "Hide"} Liked Names
+            </span>
+            {
+                // this conditionally renders the liked names list based on the showLikedNames state
+                showLikedNames && (
+                    <ul className='liked-names'>
+                        {/* this maps through the likedNames array and displays the names in a list */}
+                        {likedNames.map((name, index) => (
+                            <li key={index}>
+                                {name}
+                                <FiTrash className='delete-icon' size={15} onClick={() => setLikedNames(likedNames.filter((_, i) => i !== index))} />
+                            </li>
+                        ))}
+                    </ul>
+                )
+            }
+        </div>
+        <div className={`main-card`}>
+            {/*This conditionally renders one of the cards */}
+            {showLanding ? (
+                <div className={`fade-in ${isTransitioning ? 'fade-out' : ''}`}>
+                    <Landing handleClick={transition} />
+                </div>
+            ) : (
+                <div className={`fade-in`}>
+                    <TeamGenerator handleLike={handleLike}/>
+                </div>
+            )}
+        </div>
     </div>
   );
 }
--- src/components/TeamGenerator.js
+++ src/components/TeamGenerator.js
@@ -1,34 +1,66 @@
 import React, { useState } from 'react';
 
 import { FiHeart } from 'react-icons/fi';
-import { ADJECTIVES, MOVIES, VOWELSUFFIXES, CONSONANTSUFFIXES, PREFIXES, VOWELS } from '../constants/constants';
 
-export default function TeamGenerator() {
-    const [userInput, setUserInput] = useState('');
-    const [generatedNames, setGeneratedNames] = useState([]);
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
+import { ADJECTIVES, MOVIES, VOWELSUFFIXES, CONSONANTSUFFIXES, PREFIXES, VOWELS, NOUNS } from '../constants/constants';
+
+export default function TeamGenerator({ handleLike }) {
+    const [userInput, setUserInput] = useState(''); // state for user input
+    const [generatedNames, setGeneratedNames] = useState([]); // to store generated names to be able to display them and also to like them
+    const [selectedKeyword, setSelectedKeyword] = useState(''); // to store the selected keyword from the autocomplete
+    const [userInputSufficient, setUserInputSufficient] = useState(true); // to check if the user input is sufficient
+
+    // this function is to change the value of the user input as they are typing and also to remove the warning message if the user input becomes sufficient
     const handleChange = (e) => {
         setUserInput(e.target.value);
+        if (e.target.value?.length > 2 && !userInputSufficient) {
+            setUserInputSufficient(true);
+        }
     }
 
-    const handleGenerate = () => {
-        const randomAdjectiveName = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)] + ' ' + userInput;
-        const randomMovieTitleName = MOVIES[Math.floor(Math.random() * MOVIES.length)].replace('$', userInput);
-        console.log(randomMovieTitleName);
-        const randomPrefixName = PREFIXES[Math.floor(Math.random() * PREFIXES.length)] + ' ' + userInput;
+    // this function is to set the selected keyword from the autocomplete
+    const handleKeywordSelect = (e, value) => {
+        const safeVal = value || '';
+        setSelectedKeyword(safeVal);
+        setUserInput(safeVal);
+    }
+
+    // this function is to generate the names based on the word provided in the parameter
+    const generate = (word) => {
+        const randomAdjectiveName = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)] + ' ' + word;
+        const randomMovieTitleName = MOVIES[Math.floor(Math.random() * MOVIES.length)].replace('$', word);
+        const randomPrefixName = PREFIXES[Math.floor(Math.random() * PREFIXES.length)] + ' ' + word;
         const randomVowelName = userInput.replace(/[aeiou]/gi, VOWELS[Math.floor(Math.random() * VOWELS.length)]);
 
-        const lastLetter = userInput[userInput.length - 1];
+        const lastLetter = word[word.length - 1];
 
         var randomSuffixName = '';
 
         if (VOWELS.includes(lastLetter)) {
-            randomSuffixName = userInput + VOWELSUFFIXES[Math.floor(Math.random() * VOWELSUFFIXES.length)];
+            randomSuffixName = word + VOWELSUFFIXES[Math.floor(Math.random() * VOWELSUFFIXES.length)];
         } else {
-            randomSuffixName = userInput + CONSONANTSUFFIXES[Math.floor(Math.random() * CONSONANTSUFFIXES.length)];
+            randomSuffixName = word + CONSONANTSUFFIXES[Math.floor(Math.random() * CONSONANTSUFFIXES.length)];
         }
 
         setGeneratedNames([randomAdjectiveName, randomMovieTitleName, randomPrefixName, randomVowelName, randomSuffixName]);
+    }
+
+    // this function uses the generate function to generate names based on the user input
+    const handleGenerate = () => {
+        if (userInput && userInput?.length < 3) {
+            setUserInputSufficient(false);
+        } else {
+            generate(userInput);
+        }
+    }
+
+    // this function uses the generate function to generate names based on a random name from the NOUNS array
+    const handleGenerateRandom = () => {
+        const randomNoun = NOUNS[Math.floor(Math.random() * NOUNS.length)];
+        generate(randomNoun);
     }
 
     return (
@@ -37,19 +69,41 @@ export default function TeamGenerator() {
                 <div className="left-side">
                     <h1>Type a keyword</h1>
                     <div className="input-button">
-                        <input type="text" name="keyword" placeholder="E.g. a sport, fruit, industry, car brand, anything really!" className="keyword-input" onChange={handleChange}/>
+                    <Autocomplete
+                            freeSolo
+                            options={NOUNS}
+                            value={selectedKeyword}
+                            onChange={handleKeywordSelect}
+                            inputValue={userInput}
+                            onInputChange={handleChange}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="Keyword"
+                                    placeholder="E.g. a sport, fruit, industry, car brand, anything really!"
+                                    className="keyword-input"
+                                />
+                            )}
+                        />
+                        {/*The following is the warning sentence beneath the input field to inform the user*/}
+                        {!userInputSufficient && (
+                            <p className="warning">Your keyword should have more than two characters!</p>
+                        )}
                         <button className="generate-btn" onClick={handleGenerate}>Generate</button>
                     </div>
                     <p className="random-generation-label">Don't feel like it?</p>
-                    <button className="generate-btn">Generate Randomly</button>
+                    <button className="generate-btn" onClick={handleGenerateRandom}>Generate Randomly</button>
                 </div>
                 <div className='right-side'>
+                    {/*The following is the array of the generated names being mapped and only replacing the placeholder text of "The generated names will appear here if the user decided to generate anything"*/}
                     {generatedNames.length > 0 ? (
                         <ul className="generated-names">
                             {generatedNames.map((name, index) => (
-                                <li>
+                                <li onClick={
+                                    () => handleLike(name)
+                                }>
                                     <span>{name}</span>
-                                    <button className="like-btn"></button>
+                                    <FiHeart className='heart' size={25} />
                                 </li>
                             ))}
                         </ul>
